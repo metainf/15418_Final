7093069d59fd2ac7e0daa1a74792df0b30b774e2
Rachel Gu <regu@unix5.andrew.cmu.edu>
2017-05-09 17:06:29 -0400

    removed cuda call

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ int main( int argc, char** argv ) {
     return 1;
   }
 
-  main_test();
+//  main_test();
   string sceneFilePath = argv[optind];
   msg("Input scene file: " << sceneFilePath);
 
